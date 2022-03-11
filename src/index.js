152d0e912b3264912d11ec5376223cadfac7fbda
Hays <haysbondy@icloud.com>
2022-03-11 09:26:56 -0600

    my first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { Main } from './components/index.js'
+
+
+ReactDom.render(
+    <Router>
+        <Main />
+    </Router>,
+    document.getElementById("app")
+)
